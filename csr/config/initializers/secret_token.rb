fdf27a9ddc0040c3a0f09f92105b1dfcc9259813
Dax Panganiban <p_dax2003@yahoo.com>
2013-03-05 14:56:00 +0800

    Initial commit

---
+++ csr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csr::Application.config.secret_token = 'a5bed514da504200720eb6ecaaa16f00dc96819d3e43d64f25e9e820789478c02acff59362c2d6c2b82b4328a12d02c14338071284f980a8263aeaccabd5921d'
