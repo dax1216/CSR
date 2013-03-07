17e73f7283a46da03c4c1ad11d404d1eacbe49d2
Dax Panganiban <p_dax2003@yahoo.com>
2013-03-07 18:26:19 +0800

    Changes

--- csr/app/controllers/users_controller.rb
+++ csr/app/controllers/users_controller.rb
@@ -43,7 +43,10 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     
     if @user.save
+      sign_in @user
+      
       flash[:success] = 'Welcome to College Soccer Recruits!'
+
       redirect_to @user
     else
       render 'new'
--- csr/db/schema.rb
+++ csr/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305071850) do
+ActiveRecord::Schema.define(:version => 20130306090314) do
 
   create_table "colleges", :force => true do |t|
     t.string   "name"
@@ -27,12 +27,15 @@ ActiveRecord::Schema.define(:version => 20130305071850) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
